194e4eb2c2f51c70bb2e02805bc204201c037fef
rinku-kr <rk24736599@gmail.com>
2024-01-13 23:07:32 +0530

    Adding dotenv

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-
-// app.use(morgan('dev'));
-
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json()); // Middleware, modify incoming request!
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
