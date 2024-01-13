75fd97ea1c253c19bdb5663ab0ebbc4c78467498
rinku-kr <rk24736599@gmail.com>
2024-01-13 11:20:25 +0530

    Handling POST requests

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
+app.use(express.json()); // Middleware, modify incoming request!
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    message: 'succes',
+    status: 'succes',
     results: tours.length,
     body: {
       tours,
@@ -16,7 +17,26 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-const port = 3000;
+app.post('/api/v1/tours/', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTours = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTours);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        body: {
+          tours: newTours,
+        },
+      });
+    }
+  );
+});
+
+const port = 5000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
