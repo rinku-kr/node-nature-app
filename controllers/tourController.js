a813c41274b94c30d364934bb0899aa49948bfc9
rinku-kr <rk24736599@gmail.com>
2024-01-15 12:15:55 +0530

    Deleting documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -75,9 +75,17 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  // res.status(204).json({
-  //   status: 'success',
-  //   body: null,
-  // });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      body: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'Something fail!',
+      message: 'Something went wrong!',
+    });
+  }
 };
