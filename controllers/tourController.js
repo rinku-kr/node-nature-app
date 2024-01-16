6a0fd69075462a1e959d3d5e2a7209fea7ce893b
rinku-kr <rk24736599@gmail.com>
2024-01-16 17:54:09 +0530

    Adding fields limiting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -20,12 +20,18 @@ exports.getAllTours = async (req, res) => {
     // 2. Sorting!
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
-      console.log(sortBy);
       query = query.sort(sortBy);
     } else {
       query = query.sort('-createdAt');
     }
 
+    // 3. Field, limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
     // Execute query!
     const tours = await query;
 
