a5f09d223adc63b16e87343c0ed0241d60d528c4
rinku-kr <rk24736599@gmail.com>
2024-01-16 18:23:23 +0530

    Adding pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -32,6 +32,19 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.select('-__v');
     }
+
+    // 4. pagination(skip)
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not found!');
+    }
+
     // Execute query!
     const tours = await query;
 
