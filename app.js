70334187b8ae35dbaf1f959167450ca45bdf1cf4
rinku-kr <rk24736599@gmail.com>
2024-01-21 22:00:20 +0530

    Handling unhandled routes

--- app.js
+++ app.js
@@ -22,4 +22,11 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'Fail',
+    message: `Cannot find ${req.originalUrl} on this server!`,
+  });
+});
+
 module.exports = app;
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -29,7 +29,7 @@ class ApiFeatures {
   }
   limitFields() {
     if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(',').join(' ');
+      const fields = this.queryString.fields.split(',').join('');
       this.query = this.query.select(fields);
     } else {
       this.query = this.query.select('-__v');
