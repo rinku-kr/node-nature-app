2d25efcf359c23ae5884148e32720da1c5e9d2ff
rinku-kr <rk24736599@gmail.com>
2024-01-24 12:59:03 +0530

    Handling invalid database Ids

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,49 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDb = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 404);
+};
+
+const sendErrDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: error.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrProd = (err, res) => {
+  // Operational, trusted error: send message to client.
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: error.message,
+    });
+    // Programming or other unknown error: do not leak error details.
+  } else {
+    // Log error.
+    console.log('ERROR', err);
+
+    // Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: error.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    if (error.name === 'CastError') error = handleCastErrorDb(error);
+
+    sendErrProd(error, res);
+  }
 };
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('../models/tourModel');
 // const ApiFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 class ApiFeatures {
   constructor(query, queryString) {
@@ -75,6 +76,10 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
   // Tour.findOne({_id: req.params.id})
+
+  if (!tour) {
+    return next(new AppError('No tour find with that id', 404));
+  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -98,6 +103,9 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     new: true,
     runValidators: true,
   });
+  if (!tour) {
+    return next(new AppError('No tour find with that id', 404));
+  }
   res.status(200).json({
     status: 'success',
     body: {
@@ -107,7 +115,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id)
+  if (!tour) {
+    return next(new AppError('No tour find with that id', 404));
+  }
   res.status(204).json({
     status: 'success',
     body: null,
