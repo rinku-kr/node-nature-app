64040b0d0af9114dd40ddf90f958a04c625e4587
rinku-kr <rk24736599@gmail.com>
2024-01-24 15:18:41 +0530

    Write a user model

---
+++ models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: [true, 'Please provide your email'],
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  pasword: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minLength: 8,
+  },
+  confirmPasword: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
