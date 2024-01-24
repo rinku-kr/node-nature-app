a106ac04e6ce6107f52e83562175bedb07de812a
rinku-kr <rk24736599@gmail.com>
2024-01-24 13:34:04 +0530

    Handling unhadleRejection

--- server.js
+++ server.js
@@ -22,8 +22,16 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLE REJECTION: shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
 
 // db.tours.find({price: {$lt: 500}, rating: {$gte: 4.8}})
